e5017581bc09c303fefe1615e071dcb868e28d7e
Behiç Eren Köpüklü <eren.kopuklu@ug.bilkent.edu.tr>
2026-01-28 10:40:53 +0300

    Updated comments

--- Main.java
+++ Main.java
@@ -8,17 +8,26 @@ public class Main{
         createRandomArray(size);
 
         while(isContinue) {
-            System.out.printf("Please select an operation:\n1) %s\n2) %s\n3) %s\n4) %s\n5) %s, ","Create a new array", "", "", "", "");
-            int selection = getNumber("Please Enter the number of the operation (0 for exit): ", 0, 3);
+            System.out.printf("Please select an operation:\n1) %s\n2) %s\n3) %s\n4) %s\n5) %s\n6) %s\n0) %s","Create a new array", "Find Minimum of array", "Find maximum of array", "Find differances form average", "Sum of odd indexes", "Sum of even indexes", "Exit");
+            int selection = getNumber("Please Enter the number of the operation (0 for exit): ", 0, 6);
             if(selection == 1){
                 size = getNumber("Input an array size: ");
                 createRandomArray(size);
             }
             else if (selection == 2){
-                //secondMethod();
+                //Will find minimum of the array. Write a method.
             }
             else if(selection == 3){
-                //thirdMethod();
+                //Will find the max of the array. Write a method.
+            }
+            else if(selection == 4){
+                //Find diff. from average. Write a method.
+            }
+            else if(selection == 5){
+                //Sum of odd indexes. Write a method.
+            }
+            else if(selection == 6){
+                //Sum of even indexes. Write a method.
             }
             else if(selection == 0){
                 isContinue = false;
@@ -26,7 +35,7 @@ public class Main{
             }
         }  
     }
-
+    // gets an integer from user in the designated range
     public static int getNumber(String message, int start, int end){
         int input;
         input = getNumber(message);
@@ -42,7 +51,7 @@ public class Main{
         System.out.print(message);
         while(!scan.hasNextInt()){
             scan.nextLine();
-            System.out.println("Please enter an integer.");
+            System.out.print("Please enter an integer: ");
         }
         input = scan.nextInt();
         return input;
