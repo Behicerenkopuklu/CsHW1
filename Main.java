08364a8a64d7b44a46a1fc53f0a8348f99ed72d0
Behiç Eren Köpüklü <eren.kopuklu@ug.bilkent.edu.tr>
2026-01-28 10:30:34 +0300

    Getting input

--- Main.java
+++ Main.java
@@ -3,12 +3,15 @@ public class Main{
     static Scanner scan = new Scanner(System.in);
     static boolean isContinue = true;
     public static void main(String[] args) {
+
+        int size = getNumber("Input an array size: ");
+        createRandomArray(size);
+
         while(isContinue) {
-            System.out.printf("Please select an operation:\n1) %s\n2) %s\n3) %s\n4) %s\n5) %s, ","", "", "", "", "");
-            int selection = getNumber("Please Enter the number of the operation (0 for exit): ", 0, 5);
+            System.out.printf("Please select an operation:\n1) %s\n2) %s\n3) %s\n4) %s\n5) %s, ","Create a new array", "", "", "", "");
+            int selection = getNumber("Please Enter the number of the operation (0 for exit): ", 0, 3);
             if(selection == 1){
-                System.out.print("Input the array size");
-                int size = scan.nextInt();
+                size = getNumber("Input an array size: ");
                 createRandomArray(size);
             }
             else if (selection == 2){
@@ -17,22 +20,34 @@ public class Main{
             else if(selection == 3){
                 thirdMethod();
             }
-            else if (selection == 4){
-                fourthMethod();
-            }
-            else if(selection == 5){
-                fifthMethod();
-            }
             else if(selection == 0){
                 isContinue = false;
+                System.out.println("Terminated.");
             }
         }  
     }
 
     public static int getNumber(String message, int start, int end){
-        /* TODO */
-    } 
+        int input;
+        input = getNumber(message);
+        if(input < start || input > end){
+            System.out.printf("Please enter between %d and %d!\n", start, end);
+            input = getNumber(message, start, end);
+        }
+        return input;
+    }
 
+    public static int getNumber(String message){
+        int input;
+        System.out.print(message);
+        while(!scan.hasNextInt()){
+            scan.nextLine();
+            System.out.println("Please enter an integer.");
+        }
+        input = scan.nextInt();
+        return input;
+    }  
+    
     public static int[] createRandomArray(int size) {
         int[] arr = new int[size];
         System.out.printf("Creating array at size %d\n", size);
